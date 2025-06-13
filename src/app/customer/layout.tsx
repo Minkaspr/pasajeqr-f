f4db23f82aa06ea1fc6c5a9edb60ead2d55b3f34
AlisonNA <120201157+AlisonNA@users.noreply.github.com>
2025-06-12 23:13:01 -0500

    hola :D

--- src/app/customer/layout.tsx
+++ src/app/customer/layout.tsx
@@ -5,6 +5,7 @@ export default function CustomerLayout (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-3xl font-bold">Customer Layout</h1>
       {children}
+      Hola :D 
     </div>
   )
 }
