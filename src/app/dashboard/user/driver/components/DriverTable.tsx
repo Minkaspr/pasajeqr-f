39eb0fba41e319644afd0cc2ff4fac156a556b15
Dario Quispe <106852390+Minkaspr@users.noreply.github.com>
2025-06-25 20:25:15 -0500

    improve responsive behavior, limit visible page buttons, and enhance UI consistency

--- src/app/dashboard/user/driver/components/DriverTable.tsx
+++ src/app/dashboard/user/driver/components/DriverTable.tsx
@@ -220,7 +220,7 @@ export function DriverTable({
   })
 
   return (
-    <div className="flex w-full flex-col gap-6 px-4 lg:px-6">
+    <div className="flex w-full flex-col gap-6 lg:px-4">
       <div className="flex items-center justify-end gap-2">
         {Object.keys(rowSelection).length > 0 && (
           
